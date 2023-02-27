1e70dd5ffc483e3c2d46f3b2049bb7fa5ada8cd2
isnagil73 <inavarro@hps.es>
2023-02-27 17:56:34 +0100

    correcto

--- src/test/java/ejemplo/AppTest.java
+++ src/test/java/ejemplo/AppTest.java
@@ -6,12 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AppTest {
 	@Test
-    public void shouldAnswerWithTrue()
+    public void shouldAnswerWithTrue() throws InterruptedException
     {
         System.setProperty("webdriver.chrome.driver","chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.get("https://www.google.es/");
         driver.manage().window().maximize();
+        Thread.sleep(10000);
         driver.close();
     }
 }
