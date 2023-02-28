8ef741025fb28b1dc109d1f632759964b9ac2ea7
isnagil73 <inavarro@hps.es>
2023-02-28 16:38:16 +0100

    commit

--- src/test/java/ejemplo/AppTest.java
+++ src/test/java/ejemplo/AppTest.java
@@ -2,14 +2,14 @@ package ejemplo;
 
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AppTest {
 	@Test
     public void shouldAnswerWithTrue() throws InterruptedException
     {
-        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-        WebDriver driver=new ChromeDriver();
+        //System.setProperty("webdriver.gecko.driver","C:\\Geckodriver\\geckodriver.exe");
+        WebDriver driver=new FirefoxDriver();
         driver.get("https://www.google.es/");
         driver.manage().window().maximize();
         Thread.sleep(10000);
