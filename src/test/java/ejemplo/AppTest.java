3218900725bb8fd8ce32b8db61fdbfbd509406df
isnagil73 <inavarro@hps.es>
2023-03-01 11:07:47 +0100

    correcto

--- src/test/java/ejemplo/AppTest.java
+++ src/test/java/ejemplo/AppTest.java
@@ -3,17 +3,18 @@ package ejemplo;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class AppTest {
 	@Test
     public void shouldAnswerWithTrue() throws InterruptedException
     {
-		WebDriver driver = new ChromeDriver();
-        //System.setProperty("webdriver.gecko.driver","C:\\Geckodriver\\geckodriver.exe");
-        //WebDriver driver=new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Israel\\eclipse-workspace\\Prueba\\chromedriver.exe");
+        ChromeOptions opciones = new ChromeOptions();
+        opciones.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        WebDriver driver=new ChromeDriver(opciones);
         driver.get("https://www.google.es/");
         driver.manage().window().maximize();
-        Thread.sleep(3000);
         driver.close();
     }
 }
