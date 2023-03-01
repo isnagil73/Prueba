1801045b472171c18f6c9139345f25b546774e91
isnagil73 <inavarro@hps.es>
2023-03-01 12:15:20 +0100

    commit

--- src/test/java/ejemplo/AppTest.java
+++ src/test/java/ejemplo/AppTest.java
@@ -9,15 +9,20 @@ public class AppTest {
 	@Test
     public void shouldAnswerWithTrue() throws InterruptedException
     {
-        System.setProperty("webdriver.chrome.driver","/home/israel/Descargas/chromedriver");
+		//System.setProperty("webdriver.chrome.driver","/home/israel/Descargas/chromedriver");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Israel\\eclipse-workspace\\Prueba\\chromedriver.exe");
         ChromeOptions opciones = new ChromeOptions();
         
-        opciones.setBinary("/opt/google/chrome/chrome");
+        //opciones.setBinary("/opt/google/chrome/chrome");
+        opciones.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
+        opciones.addArguments("--no-sandbox");
+        opciones.addArguments("--disable-setuid-sandbox");
+        opciones.addArguments("--remote-debugging-port=9222");
+        opciones.addArguments("--disable-dev-shm-using");
+        opciones.addArguments("--disable-extensions");
+        opciones.addArguments("--disable-gpu");
         opciones.addArguments("start-maximized");
         opciones.addArguments("disable-infobars");
-        opciones.addArguments("--disable-extensions");
-        opciones.addArguments("--disable-dev-shm-usage");
-        opciones.addArguments("--no-sandbox");
         
         WebDriver driver=new ChromeDriver(opciones);
         driver.get("https://www.google.es/");
