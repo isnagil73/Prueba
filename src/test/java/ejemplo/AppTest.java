a5451276c0e2956cc60c60c22ea79e15ab65a053
isnagil73 <inavarro@hps.es>
2023-03-01 16:39:45 +0100

    commit

--- src/test/java/ejemplo/AppTest.java
+++ src/test/java/ejemplo/AppTest.java
@@ -1,21 +1,39 @@
 package ejemplo;
 
 import org.junit.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.By.ByCssSelector;
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.WebElement;
-import org.openqa.selenium.Proxy;
+//import org.openqa.selenium.Proxy;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class AppTest {
 	@Test
     public void prueba() throws InterruptedException
     {
+		WebDriverManager.chromedriver().setup();
 		///System.setProperty("webdriver.chrome.driver","/home/israel/Descargas/chromedriver");
-        //System.setProperty("webdriver.chrome.driver","C:\\Users\\Israel\\eclipse-workspace\\Prueba\\chromedriver.exe");
-        //ChromeOptions opciones = new ChromeOptions();
-        
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Israel\\eclipse-workspace\\Prueba\\chromedriver.exe");
+        ChromeOptions opciones = new ChromeOptions();
+        opciones.addArguments("--no-sandbox");
+        opciones.addArguments("--disable-dev-shm-usage");
+        opciones.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(opciones);
+        driver.get("http://fernando9torres-dev.hps.es:8081/actualidad");
+        //driver.get("http://httpbin.org/html");
+        //WebElement element=new WebElement();
+        String title = driver.getTitle();
+        //WebElement element = driver.findElement(By.cssSelector("p"));
+        System.out.print("El titulo de la pagina es: " + title);
+        System.out.print("");
+        driver.close();     
+                             
+        //System.out.print(driver.page_source.encode("utf-8"));
         ///opciones.setBinary("/opt/google/chrome/chrome");
 		//opciones.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
 		//opciones.addArguments("--no-sandbox");
@@ -32,7 +50,7 @@ public class AppTest {
 		//driver.close();
 				
 		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Israel\\eclipse-workspace\\Prueba\\chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver","/home/israel/Descargas/chromedriver");
+		/*System.setProperty("webdriver.chrome.driver","/home/israel/Descargas/chromedriver");
 		DesiredCapabilities capabilities = new DesiredCapabilities();
         Proxy proxy = new Proxy();
         String proxyServer = String.format("AProxyIDontWantToDisplay", System.getenv("proxy.username"), 
@@ -48,7 +66,7 @@ System.getenv("proxy.password"));
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         WebDriver driver = new ChromeDriver(options);	
         driver.get("https://www.google.es/");
-		driver.close();
+		driver.close();*/
 		
     }
 }
